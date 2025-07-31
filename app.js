f8ca4886d3b206f8f02a75301f98abb5add22a83
Leandro Avalos <leandroavalos71@gmail.com>
2025-07-31 19:26:30 -0300

    version experimental

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let amigos = [];
 let lista;
+let nombresYaSorteados = [];
 
 function agregarAmigo() {
     const input = document.querySelector('#amigo');
@@ -20,21 +21,32 @@ function actualizarListaDeAmigos() {
     lista = document.querySelector('#listaAmigos');
     lista.innerHTML = '';
     for (i=0; i < amigos.length; i++){
-        nuevoElementoLi = document.createElement('li');
+        let nuevoElementoLi = document.createElement('li');
         nuevoElementoLi.innerHTML = amigos[i];
         lista.appendChild(nuevoElementoLi);
-        console.log(i);
     }
-    console.log(amigos);
 }
 
 function sortearAmigo() {
     if (amigos.length >= 2) {
+        let listaResultados = document.querySelector('#resultado');
+        let nuevoElementoLi = document.createElement('li');
         const indice = Math.floor(Math.random() * amigos.length);
-        lista.innerHTML = '';
-        let mensajeResultado = document.querySelector('#resultado');
-        mensajeResultado.innerHTML = `El amigo sorteado es ${amigos[indice]}`
+        console.log(indice);
+        if (nombresYaSorteados.includes(amigos[indice]) && nombresYaSorteados.length < amigos.length){
+            return sortearAmigo();
+        } else if (nombresYaSorteados.length < amigos.length){
+            nombresYaSorteados.push(amigos[indice]);
+            lista.innerHTML = '';
+            listaResultados.appendChild(nuevoElementoLi);
+            nuevoElementoLi.innerHTML = `El amigo sorteado es ${amigos[indice]}`;
+        } else {
+            alert('Todos los amigos ya fueron sorteados, agregue más amigos para iniciar un nuevo sorteo');
+            nombresYaSorteados.length = 0;
+            listaResultados.innerHTML = '';
+        }
     } else {
         alert(`Ingrese ${amigos.length === 0 ? 'dos nombres' : 'otro nombre'} por favor`);
     }
+    console.log(nombresYaSorteados);
 }
