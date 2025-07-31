a5ceb190147ba47ed10137dde0713526ffcedf72
Leandro Avalos <leandroavalos71@gmail.com>
2025-07-31 17:01:51 -0300

    primera version

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let nombreAmigo;
+let lista;
+
+function agregarAmigo() {
+    nombreAmigo = document.querySelector('#amigo').value
+    if (nombreAmigo == ''){
+        alert('Por favor, ingrese un nombre valido');
+    } else {
+        document.querySelector('#amigo').value = '';
+        actualizarListaDeAmigos();
+    }
+    console.log(nombreAmigo);
+}
+
+function actualizarListaDeAmigos() {
+        if (!amigos.includes(nombreAmigo)){
+            amigos.push(nombreAmigo);
+            lista = document.querySelector('#listaAmigos');
+            let nuevoElementoLi = document.createElement('li');
+            nuevoElementoLi.innerHTML = nombreAmigo;
+            lista.appendChild(nuevoElementoLi);
+            console.log(amigos);
+        } else {
+            alert('Nombre ya ingresado, por favor ingrese otro');
+        }
+    }
+
+function sortearAmigo(){
+    if(amigos.length >= 2 ){
+        let amigoSorteado = Math.floor(Math.random()*amigos.length);
+        console.log(amigos[amigoSorteado]);
+    } else{
+        alert(`Ingrese ${amigos.length == 0 ? 'dos nombres' : 'otro nombre'} por favor`)
+    }
+}
